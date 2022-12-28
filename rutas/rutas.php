5a6ec12b3595d8d07d211fc46ac857018a7e093c
pablo <pabvelas01@gmail.com>
2022-12-28 19:30:44 -0300

    Agrega ruteo base de prueba Issue main

--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,3 +1,40 @@
 <?php 
-echo "soy la ruta";
+
+$arrayRutas=explode("/",$_SERVER["REQUEST_URI"]);
+/*
+echo "<pre>";
+ print_r( $arrayRutas);
+ echo  "</pre>";
+*/
+
+
+# arry_filter quita los indices que cuyo valor tengan vacio solo, se queda con los llenitos
+ if (count(array_filter($arrayRutas))==1){
+    $json=array(
+        "detalle"=>"No encontrado"
+     );
+    
+     echo json_encode($json,true);
+     return;
+ }
+ else if (count(array_filter($arrayRutas))==2){
+    
+    switch (strtolower(array_filter($arrayRutas)[2])){
+        case 'cursos':  $json=array(
+                            "detalle"=>"estas en la vista cursos"
+                        );
+                        break;
+        case 'registro':  $json=array(
+                            "detalle"=>"estas en la vista registros"
+                        );
+                        break;
+        
+        default:return;
+    }
+    
+     echo json_encode($json,true);
+     return;
+ }
+
+
 ?>
